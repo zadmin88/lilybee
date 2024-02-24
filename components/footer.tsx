10230bec42ac8765a26aa4444f3679d25c745785
Simon Franco Angarita <zaimon88@gmail.com>
2024-02-23 19:15:09 -0500

    Add Style to footer component

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,16 +1,36 @@
 // import { Logo } from "@/components/logo";
 import { Button } from "@/components/ui/button";
+import Image from "next/image";
+import { Facebook, Instagram, Twitter } from "lucide-react";
+import { Input } from "./ui/input";
+import { Label } from "./ui/label";
 
 export const Footer = () => {
   return (
-    <div className=" bottom-0  p-4 border-t  ">
-      <div className=" md:block  flex items-center justify-between ">
-        <Button size="sm" variant="ghost">
-          Privacy Policy
-        </Button>
-        <Button size="sm" variant="ghost">
-          Terms of Service
-        </Button>
+    <div className="bottom-0 py-16  p-4 border-t h-[400px] w-full  flex flex-col gap-8 items-center justify-between ">
+      <Image
+        src="/isotipo.png"
+        alt="Logo"
+        width={96}
+        height={81}
+        // className="h-16"
+      />
+      <Label className="text-xl">Subscribe to our newsletter</Label>
+      <Input
+        type="text"
+        placeholder="Email"
+        className="w-80 h-12 rounded-md border-2 border-primaryText"
+      />
+      <div className=" flex gap-8 ">
+        <a href="https://www.instagram.com/lilybeegentle/">
+          <Instagram size={24} />
+        </a>
+        <a href="https://www.facebook.com/profile.php?id=100086417744367">
+          <Facebook size={24} />
+        </a>
+        <a href="#">
+          <Twitter size={24} />
+        </a>
       </div>
     </div>
   );
