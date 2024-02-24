33e162793cd3eeea21521c623786dd8dd044e584
Simon Franco Angarita <zaimon88@gmail.com>
2024-02-24 18:38:53 -0500

    Add about footer

---
+++ app/(main)/about/_components/closing-text.tsx
@@ -0,0 +1,11 @@
+const ClosingText = () => {
+  return (
+    <div className="h-[150px] flex items-center font px-32 ">
+      So, there you have it, a glimpse into my world, woven with love, laughter,
+      and a sprinkle of thread magic. Come join the Lily Bee Gentle family, and
+      let&apos;s create something beautiful, together!
+    </div>
+  );
+};
+
+export default ClosingText;
--- app/(main)/about/page.tsx
+++ app/(main)/about/page.tsx
@@ -1,6 +1,8 @@
 import AboutHero from "./_components/hero";
 import AboutDescription from "./_components/about-description";
 import Quote from "@/components/quote";
+import ClosingText from "./_components/closing-text";
+import Image from "next/image";
 
 const AboutPage = () => {
   return (
@@ -11,6 +13,8 @@ const AboutPage = () => {
         style="px-48 text-center"
         text="My biggest takeaway from this wild ride? Trust the journey, savor the moments, and remember, snuggles are always the best answer."
       />
+      <ClosingText />
+      <Image src="/about-footer.png" alt="Logo" width={1400} height={240} />
     </div>
   );
 };
