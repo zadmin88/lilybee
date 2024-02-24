5cc4a0b86230ac89a29a2003ab905e0e33a343f1
Simon Franco Angarita <zaimon88@gmail.com>
2024-02-24 18:15:22 -0500

    Add about description component

--- app/(main)/about/_components/about-description.tsx
+++ app/(main)/about/_components/about-description.tsx
@@ -1,21 +1,29 @@
 const AboutDescription = () => {
   return (
-    <div className="h-[600px] flex items-center font font-semibold">
+    <div className="h-[500px] flex items-center font px-32 ">
       My love affair with crafts blossomed in Colombia, at five years old, when
       my mama gifted me my first needle and thread. Magic danced in every
       stitch, and now, it&apos;s my turn to pass that magic spark to Lily Mia!
+      <br />
+      <br />
       Life took a wild turn when I met my husband. I moved to US, learned
       English, conquered dental hygiene school, and somehow still managed to
       squeeze in some creative time. Thanks for always believing in me, honey!
       Remember that first sewing machine? A treasured memory, along with that
-      wonky-but-adorable pillow cover I made. Then came the greatest gift of
-      all: Lily Mia, born on Christmas Eve like a sprinkle of cinnamon sugar on
-      our lives. While pregnant, I made her endless handmade treasures, and
-      guess what? Lily Bee Gentle was born! It started as a cozy blanket haven
-      on Etsy, but then, like a bolt of inspiration, my world expanded. Now,
-      I&apos;m homeschooling my little explorer, building a beautiful family,
-      and crafting a fabric empire from scratch. My mission? To empower YOU,
-      fellow makers, to create your own handmade masterpieces with my
+      wonky-but-adorable pillow cover I made.
+      <br />
+      <br /> Then came the greatest gift of all: Lily Mia, born on Christmas Eve
+      like a sprinkle of cinnamon sugar on our lives. While pregnant, I made her
+      endless handmade treasures, and guess what? Lily Bee Gentle was born!
+      <br />
+      <br />
+      It started as a cozy blanket haven on Etsy, but then, like a bolt of
+      inspiration, my world expanded.
+      <br />
+      <br />
+      Now, I&apos;m homeschooling my little explorer, building a beautiful
+      family, and crafting a fabric empire from scratch. My mission? To empower
+      YOU, fellow makers, to create your own handmade masterpieces with my
       collections! Can&apos;t wait to see the magic you create!
     </div>
   );
--- app/(main)/about/page.tsx
+++ app/(main)/about/page.tsx
@@ -1,9 +1,11 @@
 import AboutHero from "./_components/hero";
+import AboutDescription from "./_components/about-description";
 
 const AboutPage = () => {
   return (
     <div className="w-full">
       <AboutHero />
+      <AboutDescription />
     </div>
   );
 };
