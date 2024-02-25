b43f508f7e3ab1958a0e1d692d6f92a647c4081c
Simon Franco Angarita <zaimon88@gmail.com>
2024-02-24 20:19:58 -0500

    Add coming soon lable to collection card

--- app/(main)/collections/_components/collection-card.tsx
+++ app/(main)/collections/_components/collection-card.tsx
@@ -1,5 +1,6 @@
 import Image from "next/image";
 import { Button } from "@/components/ui/button";
+import Link from "next/link";
 
 const CollectionCard = ({
   imageUrl,
@@ -14,7 +15,15 @@ const CollectionCard = ({
     <div className="max-w-[320px] flex flex-col gap-6">
       <Image src={imageUrl} alt="Collection Image" width={320} height={240} />
       <p>{description}</p>
-      <Button className="w-full">View all</Button>
+      {name !== "coming-soon" ? (
+        <Link href={`/collections/${name}`}>
+          <Button className="w-full">View all</Button>
+        </Link>
+      ) : (
+        <p className="rounded-lg w-full font-semibold flex items-center justify-center py-1.5 border-2 border-[#4B5666]">
+          Coming soon
+        </p>
+      )}
     </div>
   );
 };
