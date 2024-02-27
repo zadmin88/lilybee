d0e46ccd092c18eaa8b96908b8fa9975a3dc8dd2
Simon Franco Angarita <zaimon88@gmail.com>
2024-02-27 17:44:23 -0500

    Add mobile style to collections page

--- app/(main)/collections/page.tsx
+++ app/(main)/collections/page.tsx
@@ -2,7 +2,7 @@ import CollectionCard from "./_components/collection-card";
 
 const CollectionsPage = () => {
   return (
-    <div className="w-full grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 lg:gap-20 py-16 ">
+    <div className="w-full grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 lg:gap-20 gap-12 py-16 place-items-center ">
       <CollectionCard
         imageUrl="/spring-card.png"
         description="Forem ipsum dolor sit amet, consectetur adipiscing elit. "
